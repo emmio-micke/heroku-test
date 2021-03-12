877cbc7c0cfe9aa849b85d0b6de911c73962cbd8
Mikael Olsson <mikael.olsson@emmio.se>
2021-03-12 13:04:42 +0100

    Changed response to json

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-    response.end('<h1>Hello!</h1>')
+    response.json([
+        {
+            name: 'object 1',
+            message: 'lorem ipsum'
+        },
+        {
+            name: 'object 2',
+            message: 'hello world'
+        }
+    ])
 })
 
 const port = process.env.PORT || 3000;
